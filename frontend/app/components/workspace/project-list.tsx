a4211fef9159aea72ef63543963fe78472634468
schnellj4644 <schnellj4644@gmail.com>
2026-03-04 12:57:01 -0600

    Making project progress work

--- frontend/app/components/workspace/project-list.tsx
+++ frontend/app/components/workspace/project-list.tsx
@@ -1,4 +1,4 @@
-import type { Project } from "@/types";
+import { type TaskStatus, type Project } from "@/types";
 import { ProjectCard } from "../project/project-card";
 import { NoDataFound } from "../no-data-found";
 
@@ -14,6 +14,16 @@ export const ProjectList = ({
   projects,
   onCreateProject,
 }: ProjectListProps) => {
+  const calculateProgress = (project: Project) => {
+    if (project.tasks.length === 0) return 0;
+
+    const completedTasks = project.tasks.filter(
+      (task) => task.status === "Done",
+    ).length;
+
+    return Math.round((completedTasks / project.tasks.length) * 100);
+  };
+
   return (
     <div>
       <h3 className="text-xl font-medium mb-4">Projects</h3>
@@ -27,6 +37,8 @@ export const ProjectList = ({
           />
         ) : (
           projects.map((project) => {
+            project.progress = calculateProgress(project);
+
             return (
               <ProjectCard
                 key={project._id}
